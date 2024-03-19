6433e4798fa1c99d82a51b707d77fdda30ac73c9
shimaahriaz <shimaahriaz@gmail.com>
2024-03-19 02:12:19 -0700

    fixes essue

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""New class for SQLAlchemy"""
+""" new class for sqlAlchemy """
 from os import getenv
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from models.state import State
 from models.city import City
@@ -11,10 +12,11 @@ from models.place import Place
 from models.review import Review
 from models.amenity import Amenity
 
+
 class DBStorage:
-    """Create tables in environmental"""
+    """ create tables in environmental"""
     __engine = None
-    __Session = None
+    __session = None
 
     def __init__(self):
         user = getenv("HBNB_MYSQL_USER")
@@ -31,42 +33,52 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Returns a dictionary"""
-        session = self.__Session()
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
         dic = {}
         if cls:
-            if isinstance(cls, str):
+            if type(cls) is str:
                 cls = eval(cls)
-            query = session.query(cls)
-            dic = {"{}.{}".format(type(obj).__name__, obj.id): obj for obj in query}
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
         else:
-            classes = [State, City, User, Place, Review, Amenity]
-            for cls in classes:
-                query = session.query(cls)
-                dic.update({"{}.{}".format(type(obj).__name__, obj.id): obj for obj in query})
-        session.close()
-        return dic
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
-        """Add a new element in the table"""
-        self.__Session.add(obj)
+        """add a new element in the table
+        """
+        self.__session.add(obj)
 
     def save(self):
-        """Save changes"""
-        self.__Session.commit()
+        """save changes
+        """
+        self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete an element in the table"""
+        """delete an element in the table
+        """
         if obj:
-            self.__Session.delete(obj)
+            self.session.delete(obj)
 
     def reload(self):
-        """Configuration"""
+        """configuration
+        """
         Base.metadata.create_all(self.__engine)
-        self.__Session = sessionmaker(bind=self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
 
     def close(self):
-        """Closes the current session"""
-        if self.__Session:
-            self.__Session.close()
-
+        """ calls remove()
+        """
+        self.__session.close()
