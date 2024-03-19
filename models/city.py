df65957b20a35cc2e86d5494b8ea50917222013e
shimaahriaz <shimaahriaz@gmail.com>
2024-03-19 01:36:47 -0700

    a

--- models/city.py
+++ models/city.py
@@ -1,18 +1,21 @@
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+#!/usr/bin/python3
+"""This is the city class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from models.place import Place
 
-class City(BaseModel, Base):
-    """This class represents a city."""
-    __tablename__ = "cities"
 
+class City(BaseModel, Base):
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="city")
-
-    def __init__(self, *args, **kwargs):
-        """Initializes a new City."""
-        super().__init__(*args, **kwargs)
-
+                          backref="cities")
